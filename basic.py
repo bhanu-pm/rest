ad595dbf3d04abe1d63f12e1495157c68821268c
bhanu-pm <bhanuprakashm1108@gmail.com>
2025-03-29 17:00:08 -0700

    Added some basic post requests

--- basic.py
+++ basic.py
@@ -1,1 +1,36 @@
-REST basic file 
+# REST basic file 
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+class RequestStructure(BaseModel):
+	num1: float
+	num2: float
+
+@app.post("/add")
+def add(request: RequestStructure):
+	result = request.num1 + request.num2
+	return {"num1": request.num1, "num2": request.num2, "result": result}
+
+@app.post("/minus")
+def subtract(request: RequestStructure):
+	result = request.num1 - request.num2
+	return {"num1": request.num1, "num2": request.num2, "result": result}
+
+@app.post("/multiply")
+def product(request: RequestStructure):
+	result = request.num1 * request.num2
+	return {"num1": request.num1, "num2": request.num2, "result": result}
+
+@app.post("/divide")
+def division(request: RequestStructure):
+	if request.num2 != 0:
+		result = request.num1 / request.num2
+		return {"num1": request.num1, "num2": request.num2, "result": result}
+
+	raise Exception("Divide by 0 error")
+
+if __name__ == "__main__":
+	pass
